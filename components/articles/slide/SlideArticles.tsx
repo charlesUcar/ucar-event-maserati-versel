6098ada9d3fe058e943c999ff70ae403aed854ef
charlesucar <charlesucar@gmail.com>
2023-05-16 18:34:12 +0800

    feat: add swiper for slide component

--- components/articles/slide/SlideArticles.tsx
+++ components/articles/slide/SlideArticles.tsx
@@ -1,9 +1,21 @@
+import Image from 'next/image';
 import { useState } from 'react';
 import styles from './SlideArticles.module.scss';
 
 export interface ISlideArticles {
   mainTitle: string;
 }
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/scrollbar';
+
+// import required modules
+import { FreeMode, Scrollbar } from 'swiper';
 
 const SlideArticles: React.FC<ISlideArticles> = ({ mainTitle }) => {
   const [articles, setArticles] = useState([
@@ -19,28 +31,61 @@ const SlideArticles: React.FC<ISlideArticles> = ({ mainTitle }) => {
       title:
         '搭載可變色玻璃電動硬頂、2023下半年導入，Maserati正式發表MC20 Cielo',
     },
+    {
+      title: '全臺限量3輛、售價668萬，Maserati Levante Modena Trofeo Line上市',
+    },
+    {
+      title:
+        '搭載可變色玻璃電動硬頂、2023下半年導入，Maserati正式發表MC20 Cielo',
+    },
   ]);
 
-  const [isLoading, setIsLoading] = useState(false);
-
-  const fetchMoreArticles = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setArticles([
-        ...articles,
-        { title: '44444' },
-        { title: '55555' },
-        { title: '66666' },
-        { title: '77777' },
-      ]);
-      setIsLoading(false);
-    }, 2000);
+  const breakpoints = {
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
   };
 
   return (
     <section id={styles.slideArticles}>
       <div className={styles.container}>
         <div className={styles.mainTitle}>{mainTitle}</div>
+        <div className={styles.articlesContainer}>
+          <Swiper
+            slidesPerView={1}
+            spaceBetween={30}
+            breakpoints={breakpoints}
+            scrollbar={{
+              hide: false,
+            }}
+            freeMode={true}
+            modules={[Scrollbar, FreeMode]}
+            className="mySwiper"
+          >
+            {articles.map((article) => {
+              return (
+                <SwiperSlide key={article.title}>
+                  <div className={styles.articleBox}>
+                    <Image
+                      src="https://image.cache.u-car.com.tw/articleimage_1155148.jpg"
+                      width={300}
+                      height={200}
+                      alt="cover"
+                    />
+                    <div className={styles.title}>{article.title}</div>
+                  </div>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
       </div>
     </section>
   );
