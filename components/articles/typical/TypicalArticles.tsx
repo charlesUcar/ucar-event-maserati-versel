b4074ff9c675fa94c189b0f49e5e886737f739d4
charlesucar <charlesucar@gmail.com>
2023-05-20 12:14:22 +0800

    chore: test

--- components/articles/typical/TypicalArticles.tsx
+++ components/articles/typical/TypicalArticles.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState } from 'react';
 import styles from './TypicalArticles.module.scss';
 
@@ -27,47 +28,7 @@ const TypicalArticles: React.FC<ITypicalArticles> = ({ mainTitle }) => {
       title:
         '搭載可變色玻璃電動硬頂、2023下半年導入，Maserati正式發表MC20 Cielo',
     },
-    { title: '海王星特調Espresso–Maserati Grecale Trofeo義大利羅馬試駕' },
-    {
-      title:
-        '[U-EV]只有純電版、馬力上看760匹，Maserati將在2025年推出下一代Levante',
-    },
-    {
-      title: '全臺限量3輛、售價668萬，Maserati Levante Modena Trofeo Line上市',
-    },
-    {
-      title:
-        '搭載可變色玻璃電動硬頂、2023下半年導入，Maserati正式發表MC20 Cielo',
-    },
-    { title: '海王星特調Espresso–Maserati Grecale Trofeo義大利羅馬試駕' },
-    {
-      title:
-        '[U-EV]只有純電版、馬力上看760匹，Maserati將在2025年推出下一代Levante',
-    },
-    {
-      title: '全臺限量3輛、售價668萬，Maserati Levante Modena Trofeo Line上市',
-    },
-    {
-      title:
-        '搭載可變色玻璃電動硬頂、2023下半年導入，Maserati正式發表MC20 Cielo',
-    },
   ]);
-
-  const [isLoading, setIsLoading] = useState(false);
-
-  const fetchMoreArticles = () => {
-    setIsLoading(true);
-    setTimeout(() => {
-      setArticles([
-        ...articles,
-        { title: '44444' },
-        { title: '55555' },
-        { title: '66666' },
-        { title: '77777' },
-      ]);
-      setIsLoading(false);
-    }, 2000);
-  };
 
   return (
     <section id={styles.typicalArticles}>
@@ -76,7 +37,11 @@ const TypicalArticles: React.FC<ITypicalArticles> = ({ mainTitle }) => {
         <div className={styles.articlesContainer}>
           {articles.map((article) => {
             return (
-              <div className={styles.articleBox} key={article.title}>
+              <Link
+                href="/article/4/68523"
+                className={styles.articleBox}
+                key={article.title}
+              >
                 <Image
                   src="https://image.cache.u-car.com.tw/articleimage_1155148.jpg"
                   width={300}
@@ -84,7 +49,7 @@ const TypicalArticles: React.FC<ITypicalArticles> = ({ mainTitle }) => {
                   alt="cover"
                 />
                 <div className={styles.title}>{article.title}</div>
-              </div>
+              </Link>
             );
           })}
         </div>
